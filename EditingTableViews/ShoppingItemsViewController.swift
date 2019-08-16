ccdca2639daa11aae158e452a15444cf7c2b6278
NormanLiana <liananorman@pursuit.org>
2019-08-16 10:20:50 -0400

    adds TVDatasource and TVDelegate to viewDidLoad

--- EditingTableViews/ShoppingItemsViewController.swift
+++ EditingTableViews/ShoppingItemsViewController.swift
@@ -6,7 +6,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var shoppingItemsTVOutlet: UITableView!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -19,6 +19,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        shoppingItemsTVOutlet.dataSource = self
+        shoppingItemsTVOutlet.delegate = self
         
 
     }
